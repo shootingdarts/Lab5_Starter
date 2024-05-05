8eda3c9ee66aa4fe89d5218e4a1745b25d03850e
shootingdarts <103202799+shootingdarts@users.noreply.github.com>
2024-05-04 17:31:28 -0700

    Added unit tests

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Lab 5 - Starter
-
 David Liu, Joshua Villanueva
 
-[Expose](expose.html)
+## [Expose](expose.html)
+## [Explore](explore.html)
+
+### 1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+No, we would not use a unit test to test the message feature of messaging application. The fact that we are testing if a user can properly write a message and send it to another user we are looking at an outgoing message and an incoming message from two different devices. This includes the message being sent to a server or cloud and coming back down from that server cloud to the second device that is receiving the message. We can see here that this includes many different components that have to interact with each other on an application level and since one of the cons is that we cannot test how individual components are interact with each other especially on an application level this highly limits our ability to test by using a unit test with J test. 
+
+### 2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+We would use a unit test to test the "max message length" feature of the application. As this feature only involes the textbox aspect of the app, and changing other parts won't affect the non-related unit tests. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,98 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+//isPhoneNumber
+//no country code
+ test('check if this is a valid phone number', () => {
+   expect(isPhoneNumber("804-991-9494")).toBe(true);
+ });
+//added 1 at beginning/country code
+ test('check if this is a valid phone number', () => {
+  expect(isPhoneNumber("1-804-991-9494")).toBe(true);
+});
+//Not a string
+test('check if this is not a valid phone number', () => {
+  expect(isPhoneNumber(804-991-9494)).toBe(false);
+});
+//missing a trainling digit
+test('check if this is not a valid phone number', () => {
+  expect(isPhoneNumber("804-991-949")).toBe(false);
+});
+//isPhoneNumber^^^
+
+//isEmail
+//check normal email
+test('check if this is a valid email', () => {
+  expect(isEmail("j6villanueva@ucsd.edu")).toBe(true);
+});
+//check all digit email
+test('check if this is a valid email', () => {
+  expect(isEmail("404@ucsd.edu")).toBe(true);
+});
+//missing @
+test('check if this is not a valid email', () => {
+  expect(isEmail("j6villanuevaucsd.edu")).toBe(false);
+});
+//missing domain
+test('check if this is not a valid email', () => {
+  expect(isEmail("j6villanueva@")).toBe(false);
+});
+//isEmail^^^
+
+
+// isStrongPassword
+// password with 11 letters
+test('check if lstheithapk is a strong password', () => {
+  expect(isStrongPassword("lstheithapk")).toBe(true);
+});
+// password with 15 letters and numbers and underscore.
+test('check if l6sth3eitha4p_k is a strong password', () => {
+  expect(isStrongPassword("l6sth3eitha4p_k")).toBe(true);
+});
+// password with a space
+test('check if "hello world" is not a strong password', () => {
+  expect(isStrongPassword("hello world")).toBe(false);
+});
+// empty password
+test('check if "hello world" is not a strong password', () => {
+  expect(isStrongPassword("")).toBe(false);
+});
+// isStrongPassword^^^
+
+// isDate
+// MM/DD/YYYY
+test('check if 08/01/2003 is valid date', () => {
+  expect(isDate("08/01/2003")).toBe(true);
+});
+// M/D/YYYY
+test('check if 8/1/2003 is valid date', () => {
+  expect(isDate("8/1/2003")).toBe(true);
+});
+// MM/DD/YY
+test('check if 08/01/03 is valid date', () => {
+  expect(isDate("08/01/03")).toBe(false);
+});
+// M//YYYY
+test('check if 8//2003 is valid date', () => {
+  expect(isDate("8//2003")).toBe(false);
+});
+// isDate^^^
+
+// isHexColor
+// #RRGGBB
+test('check if #4287f5 is valid hexcolor', () => {
+  expect(isHexColor("#4287f5")).toBe(true);
+});
+// #RGB
+test('check if #82f is valid hexcolor', () => {
+  expect(isHexColor("#82f")).toBe(true);
+});
+// #RRGGB
+test('check if #89c42 is valid hexcolor', () => {
+  expect(isHexColor("#89c42")).toBe(false);
+});
+// RRGGBB but z is not a valid hexadecimal
+test('check if 89c42z is valid hexcolor', () => {
+  expect(isHexColor("89c42z")).toBe(false);
+});
+// isHexColor^^^
